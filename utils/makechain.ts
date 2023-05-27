8009dccf44d214e29f8bffada69979fc311cb956
b.oliinyk <bohdan.olynyk@gmail.com>
2023-05-27 16:38:09 +0100

    Some Change

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -3,7 +3,6 @@ import { PineconeStore } from 'langchain/vectorstores/pinecone';
 import { ConversationalRetrievalQAChain } from 'langchain/chains';
 
 const CONDENSE_PROMPT = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
-
 Chat History:
 {chat_history}
 Follow Up Input: {question}
